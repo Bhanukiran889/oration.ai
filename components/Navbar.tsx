77249c8a6aed4822ad8e56a931af864be22bd70b
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-09-11 13:52:28 +0530

    create a navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,12 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav style={{ display: 'flex', gap: '10px', padding: '10px' }}>
+    <nav style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '10px', padding: '10px' }}>
       <Link href="/">Home</Link>
+      <Link href="/profile">Profile</Link>
 
       <SignedIn>
         <UserButton />
-        <Link href="/profile">Profile</Link>
       </SignedIn>
 
       <SignedOut>
